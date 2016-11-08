bf7282e55587ac4c9e30d53609fdbb697f451463
Lane Schwartz <dowobeha@gmail.com>
2016-11-07 19:41:07 -0600

    Update

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "example",
     dependencies: [
-        .Package(url: "../CXmlRpc", majorVersion: 1)
+        .Package(url: "https://github.com/dowobeha/CXmlRpc.git", majorVersion: 1)
     ]
 )
