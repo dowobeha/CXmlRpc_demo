df58ffe25825329f1dca5cc7328266a50a053206
Lane Schwartz <dowobeha@gmail.com>
2016-11-08 12:30:25 -0600

    Update

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,13 @@
-import libc
-import SwiftGlibc.POSIX.termios
+//import libc
+//import SwiftGlibc.POSIX.termios
 import CXmlRpc
-import CXmlRpcAbyss
+//import CXmlRpc.server_abyss
 
 var x = CXmlRpc.XMLRPC_HAVE_WINTHREAD
 //var y = CXmlRpc.xmlrpc_server_version_major
 var y = xmlrpc_env()
 
-var z = xmlrpc_server_abyss_parms()
+var z = CXmlRpc.xmlrpc_server_abyss_parms()
 
 print(x)
 //print(y)
