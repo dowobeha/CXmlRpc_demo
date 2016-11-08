e3a058e9dde3a8563da6371090442cd992d6018c
Lane Schwartz <dowobeha@gmail.com>
2016-11-08 14:23:51 -0600

    Update

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,14 +3,33 @@
 import CXmlRpc
 //import CXmlRpc.server_abyss
 
-var x = CXmlRpc.XMLRPC_HAVE_WINTHREAD
+//var x = CXmlRpc.XMLRPC_HAVE_WINTHREAD
 //var y = CXmlRpc.xmlrpc_server_version_major
-var y = xmlrpc_env()
+//var y = xmlrpc_env()
 
-var z = CXmlRpc.xmlrpc_server_abyss_parms()
 
-print(x)
+//var registryP : UnsafeMutablePointer<CXmlRpc.xmlrpc_registry>? = nil
+//var r = xmlrpc_registry()
+
+
+//print(x)
 //print(y)
 
-CXmlRpc.xmlrpc_term()
+//CXmlRpc.xmlrpc_term()
 
+var serverparm = xmlrpc_server_abyss_parms()
+var env = xmlrpc_env()
+
+print(serverparm)
+print()
+print(env)
+print()
+xmlrpc_env_init(&env)
+print(env)
+//var registryP : UnsafeMutablePointer<CXmlRpc.xmlrpc_server_abyss_parms>? = nil
+var registryP = xmlrpc_registry_new(&env)
+//print(registryP)
+
+func sample_add() -> UnsafeMutablePointer<xmlrpc_value>? {
+   return nil
+}
--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clear; rm -rf .build Packages ; swift build -Xlinker -L/usr/lib/x86_64-linux-gnu -Xcc -I/usr/include && echo && ./.build/debug/example
+clear; rm -rf .build Packages ; swift build -Xlinker -L/usr/lib/x86_64-linux-gnu -Xcc -I/usr/include -Xcc -I/usr/include/x86_64-linux-gnu && echo && ./.build/debug/example
